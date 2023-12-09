3cd7a7d72f67961dbdf5cae0cc16d3bcf8df3369
szymonhel <szhelmecki@gmail.com>
2023-12-09 15:56:09 +0100

    add new key button

--- src/app/_components/FileTable.tsx
+++ src/app/_components/FileTable.tsx
@@ -4,15 +4,17 @@ import {Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, Table
 import React from 'react';
 import {FileContent, FileContentSerialized} from '@/types/uploaded-file';
 import {Button} from "@/components/ui/button";
-import {Trash2} from "lucide-react";
+import {Pencil, Trash2} from "lucide-react";
 import CustomDialog from "@/components/ui/custom/CustomDialog";
+import KeyForm, {CreatedKey} from "@/app/_components/KeyForm";
 
 export type FileTableProps = {
     fileContentCollection: FileContentSerialized[];
     uniqueKeys: string[];
+    editKey: (model: CreatedKey) => void;
     removeKey: (key: string) => void;
 }
-const FileTable = ({fileContentCollection, uniqueKeys, removeKey}: FileTableProps) => {
+const FileTable = ({fileContentCollection, uniqueKeys, removeKey, editKey}: FileTableProps) => {
 
 
     return (
@@ -38,7 +40,12 @@ const FileTable = ({fileContentCollection, uniqueKeys, removeKey}: FileTableProp
                                 <TableCell key={index}>{content[key] ?? '-'} </TableCell>
                             )}
                             <TableCell>
-                                <div className={'w-100 flex justify-end'}>
+                                <div className={'w-100 flex justify-end gap-2'}>
+                                    <CustomDialog title={'New Key'} trigger={
+                                        <Button variant={'outline'} size={'icon'} color={'warning'}><Pencil className="h-4 w-4" /></Button>
+                                    }
+                                                  content={<KeyForm fileNames={fileContentCollection.map(z => z.name)} alreadyCreatedKeys={uniqueKeys}
+                                                                    onSubmit={editKey}/>}/>
                                 <CustomDialog trigger={
                                     <Button variant="outline" size="icon">
                                         <Trash2 className="h-4 w-4" />
