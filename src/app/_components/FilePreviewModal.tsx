f053b40fde3653dc7a2cb71010ce771674fbba83
szymonhel <szhelmecki@gmail.com>
2023-12-14 22:00:47 +0100

    fix build

--- src/app/_components/FilePreviewModal.tsx
+++ src/app/_components/FilePreviewModal.tsx
@@ -62,17 +62,10 @@ const FilePreviewModal: FC<FilePreviewModalProps> = ({filesContent, uniqueKeys})
         el.style.position = "absolute";
         el.style.left = "-9999px";
         document.body.appendChild(el);
-        const selected =
-            document.getSelection().rangeCount > 0
-                ? document.getSelection().getRangeAt(0)
-                : false;
+        el.focus();
         el.select();
-        document.execCommand("copy");
+        document.execCommand('copy');
         document.body.removeChild(el);
-        if (selected) {
-            document.getSelection().removeAllRanges();
-            document.getSelection().addRange(selected);
-        }
         toast({
             description: "Content copied",
         });
