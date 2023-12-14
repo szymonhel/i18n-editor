ed453fd356a518a0caa9a1575ff2bbab7c55b091
szymonhel <szhelmecki@gmail.com>
2023-12-14 21:37:26 +0100

    Fixes #3

--- src/app/_components/FilePreviewModal.tsx
+++ src/app/_components/FilePreviewModal.tsx
@@ -33,6 +33,31 @@ const FilePreviewModal: FC<FilePreviewModalProps> = ({filesContent, uniqueKeys})
         return result;
     }
 
+    function convertNumericKeysToArray(obj: any) {
+        for (const key in obj) {
+            if (Object.prototype.hasOwnProperty.call(obj, key)) {
+                if(key === 'goodMorning') {
+                    //debugger;
+                }
+                if (typeof obj[key] === 'object' && !Object.keys(obj[key]).every(k => !isNaN(Number(k)))) {
+                    obj[key] = convertNumericKeysToArray(obj[key]);
+                } else {
+                    const numericKeys = Object.keys(obj[key]).every(k => !isNaN(Number(k)));
+                    if (numericKeys && typeof obj[key] !== 'string') {
+                        obj[key] = Object.values(obj[key]);
+                    }
+                }
+            }
+        }
+        return obj;
+    }
+
+    downloadFile(data: Response) {
+        const blob = new Blob([data], { type: 'text/csv' });
+        const url= window.URL.createObjectURL(blob);
+        window.open(url);
+    }
+    
     function allKeys() {
         return uniqueKeys.reduce((acc, item, index) => {
             acc[item] = ``;
@@ -40,7 +65,8 @@ const FilePreviewModal: FC<FilePreviewModalProps> = ({filesContent, uniqueKeys})
         }, {} as Record<string, string>);
     }
 
-    const copyVal = filesContent.map(z => ({...z, content: unflattenObject({...allKeys(), ...z.content})}));
+    const copyVal = filesContent.map(z => ({...z, content: unflattenObject({...allKeys(), ...z.content})})).map(z => ({...z, content: convertNumericKeysToArray(z.content)}));
+    console.log(copyVal);
     return (
         <>
         <Tabs defaultValue="account" className=" w-[100%]">
